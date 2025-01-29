ad0a1a0c8afa6031c11a11358a5982f12eb12472
Shamoil Rao <53srao786@gmail.com>
2025-01-29 12:25:53 +0530

    Lesson 8

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[float] = None
 
 @app.get("/")
 async def root():
@@ -12,11 +20,13 @@ async def get_posts():
     return {"data":"These are all the posts."}
 
 @app.post("/createposts")
-async def create_post(payload: dict = Body(...)):
+async def create_post(post: Post):
     return {
         "msg":"post created successfully.", 
         "data": {
-            "title": payload["title"], 
-            "content": payload["content"]
+            "title": post.title, 
+            "content": post.content,
+            "rating": post.rating,
+            "published": post.published
             }
         }
---
+++ readme.md
@@ -0,0 +1,5 @@
+lesson 8:
+ -imported Base model from pydantic.
+ -created class Post which extends BaseModel and defined schema for create_post method by declaring title as string, content as string, published as bool defaulting to true, and rating to optional int oterwise None.
+ -changed create_post methods parameter to accept a object of type Post and store it in argument named post, returned different values of this object.
+---------------------------------------------------------------------------------------------------------
